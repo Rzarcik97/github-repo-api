9d685273739c13994891ec0f72b6ef430f399ee4
Karol Gajda <karolgaj97@interia.pl>
2026-06-03 15:47:30 +0200

    Initialize Spring Boot project

---
+++ src/main/java/demo/githubrepoapi/GithubRepoApiApplication.java
@@ -0,0 +1,13 @@
+package demo.githubrepoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubRepoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/demo/githubrepoapi/GithubRepoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.githubrepoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
